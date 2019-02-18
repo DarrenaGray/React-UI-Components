14930bd060999d30e3e6e67864f951047fa406a1
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-02-18 17:46:23 -0500

    Imported header and card container components to index.js

--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<HeaderContainer />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HeaderContainer />, document.getElementById('header'));
+ReactDOM.render(<CardContainer />, document.getElementById('card'));
