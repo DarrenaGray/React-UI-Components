db62a07eeaa61906331eb7136164c905d232dfb9
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-02-19 19:38:48 -0500

    Added outline for clear and zero buttons

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -4,9 +4,9 @@ import './Button.css';
 const NumberButton = () => {
     return  (
     <div className="calc-buttons">
+    <div className="clear-btn"></div>
         <div className="number-buttons">
-            <div className="box"></div>
-            <div className="box"></div>
+             <div className="box"></div>
             <div className="box"></div>
             <div className="box"></div>
             <div className="box"></div>
@@ -16,6 +16,7 @@ const NumberButton = () => {
             <div className="box"></div>
             <div className="box"></div>
         </div>
+        <div className="zero-btn"></div>
     </div>
     );
 };
